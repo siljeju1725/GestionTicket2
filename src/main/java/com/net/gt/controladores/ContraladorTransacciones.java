637d25a7d90596df9dafbcfc5277985b6d22e23d
siljeju1725 <siljeju1725@gmail.com>
2021-04-02 18:41:38 -0500

    creacion conexion

--- src/main/java/com/net/gt/controladores/ContraladorTransacciones.java
+++ src/main/java/com/net/gt/controladores/ContraladorTransacciones.java
@@ -1,5 +1,19 @@
 package com.net.gt.controladores;
 
+import java.util.List;
+
+import com.net.gt.modelos.Tickets;
+
 public interface ContraladorTransacciones {
+	
+	public List<Tickets> obtenerDatos();
+	
+	public Tickets obtenerPersona(int id);
+	
+	public void insertar (Tickets p);
+	
+	public void actualizar (Tickets p);
+	
+	public void eliminar (int id);
 
 }
---
+++ src/main/java/com/net/gt/controladores/Controlador.java
@@ -0,0 +1,39 @@
+package com.net.gt.controladores;
+
+import java.util.List;
+
+import com.net.gt.modelos.Tickets;
+
+public class Controlador implements ContraladorTransacciones  {
+
+	@Override
+	public List<Tickets> obtenerDatos() {
+		
+		return null;
+	}
+
+	@Override
+	public Tickets obtenerPersona(int id) {
+		 
+		return null;
+	}
+
+	@Override
+	public void insertar(Tickets p) {
+		
+		
+	}
+
+	@Override
+	public void actualizar(Tickets p) {
+		
+		
+	}
+
+	@Override
+	public void eliminar(int id) {
+		
+		
+	}
+
+}
